50672fd0402c0dd308480d4905a0b37b684a71d0
Dracula <captain@fantasyui.com>
2016-11-06 09:31:24 -0500

    Update unit test

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-require(__dirname+'/index.js')({css:'.flarp::a .o {background: red;} .xxx::y, .z {background: red;}  .xxx::y > .z {background: red;}'}).then(result=>{
-  if(JSON.stringify(result.selectors) === `{".flarp":true,".o":true,".xxx":true,".z":true}`){
+require(__dirname+'/index.js')({css:`a[href^=/].daisy, .daisy:has(nav) > a[href]:lt($var).give.me.your.answer.do {content:' ';} .im.half.crazy::a .all.for {background: red;} .the::y, .love {background: red;}  .of::y > .you {background: red;}`}).then(result=>{
+  if(result.classNames.join(" ") === "daisy give me your answer do im half crazy all for the love of you"){
     process.exit();
   }else{
     process.exit(1);
